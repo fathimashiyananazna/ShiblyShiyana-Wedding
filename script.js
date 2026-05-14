c7c95e10a7669b81d778bc9ef750a38d4cc6f4a6
fathimashiyananazna <fathimashiyananazna@gmail.com>
2026-05-14 23:52:32 +0530

    Add files via upload
    initial upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const weddingDate =
+new Date("Dec 20, 2026 10:00:00").getTime();
+
+const countdownFunction = setInterval(() => {
+
+  const now = new Date().getTime();
+
+  const distance = weddingDate - now;
+
+  const days = Math.floor(
+    distance / (1000 * 60 * 60 * 24)
+  );
+
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) /
+    (1000 * 60 * 60)
+  );
+
+  const minutes = Math.floor(
+    (distance % (1000 * 60 * 60)) /
+    (1000 * 60)
+  );
+
+  const seconds = Math.floor(
+    (distance % (1000 * 60)) /
+    1000
+  );
+
+  document.getElementById("countdown").innerHTML =
+    ${days}d ${hours}h ${minutes}m ${seconds}s;
+
+  if(distance < 0){
+
+    clearInterval(countdownFunction);
+
+    document.getElementById("countdown").innerHTML =
+      "Wedding Day ❤️";
+
+  }
+
+},1000);
